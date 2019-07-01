6fbfc115fee9460d847dd72aafc9c8caf7265bf5
daishi <daishi@axlight.com>
2019-07-01 18:54:40 +0900

    simple counter example for several frameworks

---
+++ examples/counter-react-tracked/src/App.js
@@ -0,0 +1,38 @@
+import React, { useReducer } from 'react';
+import { Provider, useTracked } from 'react-tracked';
+
+const initialState = {
+  count: 0,
+  text: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment': return { ...state, count: state.count + 1 };
+    case 'decrement': return { ...state, count: state.count - 1 };
+    case 'setText': return { ...state, text: action.text };
+    default: return state;
+  }
+};
+
+const useValue = () => useReducer(reducer, initialState);
+
+const Counter = () => {
+  const [state, dispatch] = useTracked();
+  return (
+    <div>
+      <span>Count: {state.count}</span>
+      <button type="button" onClick={() => dispatch({ type: 'increment' })}>+1</button>
+      <button type="button" onClick={() => dispatch({ type: 'decrement' })}>-1</button>
+    </div>
+  );
+};
+
+const App = () => (
+  <Provider useValue={useValue}>
+    <Counter />
+    <Counter />
+  </Provider>
+);
+
+export default App;
