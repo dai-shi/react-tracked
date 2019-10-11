b20a66d79283824fa4ae7f1dd118b315e54b6a81
daishi <daishi@axlight.com>
2019-10-11 22:50:39 +0900

    add some comments to deep proxy utils

--- src/index.d.ts
+++ src/index.d.ts
@@ -16,5 +16,17 @@ export const createContainer: <State, Update, Props>(
 
 // deep proxy utils
 
+/**
+ * If `obj` is a proxy, it will mark the entire object as used.
+ * Otherwise, it does nothing.
+ */
 export const trackMemo: (obj: unknown) => void;
+
+/**
+ * If `obj` is a proxy, it will return the original object.
+ * Otherwise, it will return the same value.
+ *
+ * Note:
+ * One can test with `x !== getUntrackedObject(x)` to see if `x` is a proxy.
+ */
 export const getUntrackedObject: <T>(obj: T) => T;
