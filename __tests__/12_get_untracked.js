348a5399e2c3eb1784486a00046dde68fcaf4d21
daishi <daishi@axlight.com>
2019-10-11 12:30:10 +0900

    add test for getUntracked

---
+++ __tests__/12_get_untracked.js
@@ -0,0 +1,19 @@
+import { createDeepProxy, getUntracked } from '../src/deepProxy';
+
+const noop = () => {};
+
+describe('object tracking', () => {
+  it('should get untracked object', () => {
+    const proxyCache = new WeakMap();
+    const s1 = { a: { b: 1, c: 2 } };
+    const a1 = new WeakMap();
+    const p1 = createDeepProxy(s1, a1, proxyCache);
+    noop(p1.a.b);
+    expect(p1).not.toBe(s1);
+    expect(p1.a).not.toBe(s1.a);
+    expect(p1.a.b).toBe(s1.a.b);
+    expect(getUntracked(p1)).toBe(s1);
+    expect(getUntracked(p1.a)).toBe(s1.a);
+    expect(getUntracked(p1.a.b)).toBe(s1.a.b);
+  });
+});
