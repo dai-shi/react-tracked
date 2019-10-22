97448cd681c9f825e4b7b20803d94f4af29421b7
daishi <daishi@axlight.com>
2019-10-22 16:02:29 +0900

    better async example with enhancing dispatch in useValue

--- examples/12_async/src/store.ts
+++ examples/12_async/src/store.ts
@@ -55,46 +55,38 @@ const reducer: Reducer<State, InnerAction | OuterAction> = (state, action) => {
   }
 };
 
-const useValue = () => useReducer(reducer, initialState);
-
-const {
-  Provider,
-  useTrackedState,
-  useUpdate: useInnerDispatch,
-} = createContainer(useValue);
-
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 type AsyncAction =
   | { type: 'FETCH_PERSON'; id: number }
   | { type: 'DELAYED_CLEAR' };
 
-const useDispatch = () => {
-  const innerDispatch = useInnerDispatch();
+const useValue = () => {
+  const [state, rawDispatch] = useReducer(reducer, initialState);
   const dispatch = useCallback(async (action: AsyncAction | OuterAction) => {
     if (action.type === 'FETCH_PERSON') {
-      innerDispatch({ type: 'START_FETCH' });
+      rawDispatch({ type: 'START_FETCH' });
       try {
         const response = await fetch(`https://reqres.in/api/users/${action.id}?delay=1`);
         const data = await response.json();
         const firstName = data.data.first_name;
         if (typeof firstName !== 'string') throw new Error();
-        innerDispatch({ type: 'FINISH_FETCH', firstName });
+        rawDispatch({ type: 'FINISH_FETCH', firstName });
       } catch (e) {
-        innerDispatch({ type: 'ERROR_FETCH' });
+        rawDispatch({ type: 'ERROR_FETCH' });
       }
     } else if (action.type === 'DELAYED_CLEAR') {
       await sleep(500);
-      innerDispatch({ type: 'CLEAR_FIRST_NAME' });
+      rawDispatch({ type: 'CLEAR_FIRST_NAME' });
     } else {
-      innerDispatch(action);
+      rawDispatch(action);
     }
-  }, [innerDispatch]);
-  return dispatch;
+  }, []);
+  return [state, dispatch] as [typeof state, typeof dispatch];
 };
 
-export {
+export const {
   Provider,
   useTrackedState,
-  useDispatch,
-};
+  useUpdate: useDispatch,
+} = createContainer(useValue);
