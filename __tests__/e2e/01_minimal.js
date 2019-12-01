c404623e29a9e1aa259068f2234cc6facc4146bc
daishi <daishi@axlight.com>
2019-12-02 00:13:54 +0900

    use innerHTML instead of screenshot

--- __tests__/e2e/01_minimal.js
+++ __tests__/e2e/01_minimal.js
@@ -1,74 +1,71 @@
 /* global page */
 
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
-
 const port = process.env.PORT || '8080';
 jest.setTimeout(15 * 1000);
-expect.extend({ toMatchImageSnapshot });
 
 describe('01_minimal', () => {
   it('should work with recorded events', async () => {
     await page.goto(`http://localhost:${port}/`);
 
-    await page.setViewport({ width: 1129, height: 324 });
+    await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(2) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(2)');
     await page.click('body > #app > div:nth-child(2) > div > button:nth-child(2)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(2)');
     await page.click('body > #app > div:nth-child(2) > div > button:nth-child(2)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
-
-    await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(2)');
-    await page.click('body > #app > div:nth-child(2) > div > button:nth-child(2)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(2) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(2) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(2) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
     await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
     await page.waitForSelector('body > #app > div:nth-child(5) > div > input');
     await page.click('body > #app > div:nth-child(5) > div > input');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.type('body > #app > div:nth-child(5) > div > input', '1');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.type('body > #app > div:nth-child(5) > div > input', '2');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.type('body > #app > div:nth-child(5) > div > input', '3');
-    expect(await page.screenshot()).toMatchImageSnapshot();
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
   });
 });
