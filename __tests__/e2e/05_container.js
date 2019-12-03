bd8d9afd8e7b4ca3758b7135f1d1fbb5a2aae500
daishi <daishi@axlight.com>
2019-12-03 23:03:19 +0900

    add e2e 05

---
+++ __tests__/e2e/05_container.js
@@ -0,0 +1,90 @@
+/* global page */
+
+const port = process.env.PORT || '8080';
+jest.setTimeout(15 * 1000);
+
+describe('05_container', () => {
+  it('should work with recorded events', async () => {
+    await page.goto(`http://localhost:${port}/`);
+
+    await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(2) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(2) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(2) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(2) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(3) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(5) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(5) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(5) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(5) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(5) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(5) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(2)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.waitForSelector('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    await page.click('body > #app > div:nth-child(6) > div > button:nth-child(3)');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+  });
+});
--- examples/05_container/src/Counter.tsx
+++ examples/05_container/src/Counter.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { useValue } from './state';
 
+let numRendered = 0;
+
 const Counter: React.FC<{ useTracked: typeof useValue }> = ({ useTracked }) => {
   const [state, dispatch] = useTracked();
   return (
     <div>
-      {Math.random()}
+      numRendered: {++numRendered}
       <div>
         <span>Count: {state.count}</span>
         <button type="button" onClick={() => dispatch({ type: 'increment' })}>+1</button>
