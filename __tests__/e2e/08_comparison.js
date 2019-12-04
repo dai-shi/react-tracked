7564b23667cbf77b53ddf1876f73a8c87d633a3e
daishi <daishi@axlight.com>
2019-12-04 12:34:21 +0900

    add e2e 08

---
+++ __tests__/e2e/08_comparison.js
@@ -0,0 +1,44 @@
+/* global page */
+
+const port = process.env.PORT || '8080';
+jest.setTimeout(15 * 1000);
+
+describe('08_comparison', () => {
+  it('should work with recorded events', async () => {
+    await page.goto(`http://localhost:${port}/`);
+
+    await page.waitForSelector('body > #app > div:nth-child(2) > input');
+    await page.click('body > #app > div:nth-child(2) > input');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(2) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(3) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(5) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(6) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(8) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(9) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(11) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(12) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(14) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+
+    await page.type('body > #app > div:nth-child(15) > input', '1');
+    expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
+  });
+});
