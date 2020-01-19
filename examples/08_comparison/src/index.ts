844c09cf595ab79384cf5ac3f3e65422af42fe56
daishi <daishi@axlight.com>
2020-01-19 13:50:55 +0900

    use react-dom experimental typing

--- examples/08_comparison/src/index.ts
+++ examples/08_comparison/src/index.ts
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore
-  createRoot,
-} from 'react-dom';
+import { createRoot } from 'react-dom';
 
 import App from './App';
 
