9a18ff615936a87384d24093b9181e20bbcde91f
daishi <daishi@axlight.com>
2020-03-12 14:07:18 +0900

    add a comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { createContainer } from './createContainer';
 export { trackMemo, getUntrackedObject } from 'proxy-compare';
 
+// undocumented APIs
 export { createProvider } from './createProvider';
 export { useUpdate } from './useUpdate';
 export { useTrackedState, useTracked } from './useTrackedState';
