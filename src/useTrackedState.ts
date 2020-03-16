4b11fc3c049120254df7efe91e6d2806dda0018d
daishi <daishi@axlight.com>
2020-03-16 12:00:07 +0900

    cleaner useTrackedState without refs/effects

--- src/useTrackedState.ts
+++ src/useTrackedState.ts
@@ -3,12 +3,10 @@
 import {
   Context,
   MutableRefObject,
-  useCallback,
   useContext,
   useMemo,
   // @ts-ignore
   useMutableSource,
-  useRef,
 } from 'react';
 import {
   createDeepProxy,
@@ -18,7 +16,7 @@ import {
   MODE_ASSUME_UNCHANGED_IF_UNAFFECTED_IN_DEEP,
 } from 'proxy-compare';
 
-import { useIsomorphicLayoutEffect, useAffectedDebugValue } from './utils';
+import { useAffectedDebugValue } from './utils';
 import {
   ContextValue,
   MUTABLE_SOURCE_CONTEXT_PROPERTY,
@@ -34,11 +32,6 @@ const MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED = (
 const MODE_MUTABLE_ROOT_STATE = MODE_IGNORE_REF_EQUALITY; // only for root
 const MODE_DEFAULT = MODE_ASSUME_UNCHANGED_IF_UNAFFECTED; // only for root
 
-const STATE_PROPERTY = 's';
-const AFFECTED_PROPERTY = 'a';
-const CACHE_PROPERTY = 'c';
-const DEEP_PROXY_MODE_PROPERTY = 'd';
-
 type Opts = any; // TODO types
 
 export const useTrackedState = <State, Update>(
@@ -49,49 +42,42 @@ export const useTrackedState = <State, Update>(
     [MUTABLE_SOURCE_CONTEXT_PROPERTY]: mutableSource,
   } = useContext(CustomContext);
   const affected = new WeakMap();
-  const lastTracked = useRef<any>(); // TODO types
-  const getSnapshot = useCallback(
-    (source: {
+  const deepChangedMode = (
+    /* eslint-disable no-nested-ternary, indent, no-multi-spaces */
+      opts.unstable_forceUpdateForStateChange     ? MODE_ALWAYS_ASSUME_CHANGED_IF_UNAFFECTED
+    : opts.unstable_ignoreIntermediateObjectUsage ? MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED
+    : opts.unstable_ignoreStateEquality           ? MODE_MUTABLE_ROOT_STATE
+    : /* default */                                 MODE_DEFAULT
+    /* eslint-enable no-nested-ternary, indent, no-multi-spaces */
+  );
+  const getSnapshot = useMemo(() => {
+    let prevState: State | null = null;
+    const deepChangedCache = new WeakMap();
+    return (source: {
       [STATEREF_SOURCE_PROPERTY]: MutableRefObject<State>;
     }) => {
       const nextState = source[STATEREF_SOURCE_PROPERTY].current;
-      const lastTrackedCurrent = lastTracked.current;
-      if (!lastTrackedCurrent) return nextState;
-      if (lastTrackedCurrent[AFFECTED_PROPERTY] !== affected) return nextState;
-      if (lastTrackedCurrent[STATE_PROPERTY] === nextState) return nextState;
-      if (isDeepChanged(
-        lastTrackedCurrent[STATE_PROPERTY],
+      if (prevState !== null && prevState !== nextState && !isDeepChanged(
+        prevState,
         nextState,
-        lastTrackedCurrent[AFFECTED_PROPERTY],
-        lastTrackedCurrent[CACHE_PROPERTY],
-        lastTrackedCurrent[DEEP_PROXY_MODE_PROPERTY],
-      )) return nextState;
-      // not changed
-      return lastTrackedCurrent[STATE_PROPERTY];
-    },
-    [affected],
-  );
-  const state: State = useMutableSource(mutableSource, getSnapshot, subscribe);
-  useIsomorphicLayoutEffect(() => {
-    lastTracked.current = {
-      [STATE_PROPERTY]: state,
-      [AFFECTED_PROPERTY]: affected,
-      [CACHE_PROPERTY]: new WeakMap(),
-      /* eslint-disable no-nested-ternary, indent */
-      [DEEP_PROXY_MODE_PROPERTY]:
-      opts.unstable_forceUpdateForStateChange ? MODE_ALWAYS_ASSUME_CHANGED_IF_UNAFFECTED
-      : opts.unstable_ignoreIntermediateObjectUsage ? MODE_ALWAYS_ASSUME_UNCHANGED_IF_UNAFFECTED
-      : opts.unstable_ignoreStateEquality ? MODE_MUTABLE_ROOT_STATE
-      : /* default */ MODE_DEFAULT,
-      /* eslint-enable no-nested-ternary, indent */
+        affected,
+        deepChangedCache,
+        deepChangedMode,
+      )) {
+        // not changed
+        return prevState;
+      }
+      prevState = nextState;
+      return nextState;
     };
-  });
+  }, [affected, deepChangedMode]);
+  const state: State = useMutableSource(mutableSource, getSnapshot, subscribe);
   if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useAffectedDebugValue(state, affected);
   }
-  const proxyCache = useRef(new WeakMap()); // per-hook proxyCache
-  return createDeepProxy(state, affected, proxyCache.current);
+  const proxyCache = useMemo(() => new WeakMap(), []); // per-hook proxyCache
+  return createDeepProxy(state, affected, proxyCache);
 };
 
 export const useTracked = <State, Update>(
