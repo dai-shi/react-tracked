f9470b8e903dbc21e8fddb45e7fc8ec68e6049ca
daishi <daishi@axlight.com>
2020-07-02 21:06:55 +0900

    remove dist

--- dist/src/createContainer.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { useTrackedState as useTrackedStateOrig, useTracked as useTrackedOrig } from './useTrackedState';
-export declare const createContainer: <State, Update extends (...args: any) => any, Props>(useValue: (props: Props) => readonly [State, Update]) => {
-    readonly Provider: FC<Props>;
-    readonly useTrackedState: (opts?: Parameters<typeof useTrackedStateOrig>[1]) => State;
-    readonly useTracked: (opts?: Parameters<typeof useTrackedOrig>[2]) => [State, Update];
-    readonly useUpdate: () => Update;
-    readonly useSelector: <Selected>(selector: (state: State) => Selected) => Selected;
-};
--- dist/src/utils.d.ts
+++
@@ -1,3 +0,0 @@
-declare type Obj = Record<string, unknown>;
-export declare const useAffectedDebugValue: <State>(state: State, affected: WeakMap<Obj, Set<string>>) => void;
-export {};
