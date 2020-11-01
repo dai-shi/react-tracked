66bb45f2ecdf4248ec109d079f5122f87b5d4227
daishi <daishi@axlight.com>
2020-11-01 21:11:03 +0900

    update proxy-compare v1.0.3 and simplify utils

--- src/utils.ts
+++ src/utils.ts
@@ -3,33 +3,15 @@ import {
   useRef,
   useDebugValue,
 } from 'react';
+import { affectedToPathList } from 'proxy-compare';
 
 type Obj = Record<string, unknown>;
 
-const affectedToPathList = <State>(
-  state: State,
-  affected: WeakMap<Obj, Set<string>>,
-) => {
-  const list: string[][] = [];
-  const walk = (obj: unknown, path?: string[]) => {
-    const used = affected.get(obj as Obj);
-    if (used) {
-      used.forEach((key) => {
-        walk((obj as { [k: string]: Obj })[key], path ? [...path, key] : [key]);
-      });
-    } else if (path) {
-      list.push(path);
-    }
-  };
-  walk(state);
-  return list;
-};
-
 export const useAffectedDebugValue = <State>(
   state: State,
-  affected: WeakMap<Obj, Set<string>>,
+  affected: WeakMap<Obj, unknown>,
 ) => {
-  const pathList = useRef<string[][]>();
+  const pathList = useRef<(string | number | symbol)[][]>();
   useEffect(() => {
     pathList.current = affectedToPathList(state, affected);
   });
