758923a82ea29acfee22897ce003e5c394735fb1
daishi <daishi@axlight.com>
2020-12-04 00:17:04 +0900

    update configs

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,11 @@ const { DIR, EXT = 'ts' } = process.env;
 
 module.exports = {
   mode: 'development',
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
   entry: `./examples/${DIR}/src/index.${EXT}`,
+  output: {
+    publicPath: '/',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: `./examples/${DIR}/public/index.html`,
@@ -14,19 +17,12 @@ module.exports = {
   ],
   module: {
     rules: [{
-      test: /\.jsx?$/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-          ],
-        },
-      }],
-    }, {
-      test: /\.tsx?$/,
+      test: /\.[jt]sx?$/,
+      exclude: /node_modules/,
       loader: 'ts-loader',
+      options: {
+        transpileOnly: true,
+      },
     }],
   },
   resolve: {
@@ -38,5 +34,6 @@ module.exports = {
   devServer: {
     port: process.env.PORT || '8080',
     contentBase: `./examples/${DIR}/public`,
+    historyApiFallback: true,
   },
 };
