5113b9784ff9afafd856e6dee44423fbace90d06
daishi <daishi@axlight.com>
2020-12-18 21:41:26 +0900

    add new example 14

---
+++ examples/14_dynamic/src/Counter.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import { useTracked } from './state';
+
+let numRendered = 0;
+
+const Counter: React.FC = () => {
+  const [index, setIndex] = useState(0);
+  const [state, dispatch] = useTracked();
+  return (
+    <div>
+      numRendered: {++numRendered}
+      <div>
+        <span>Count: {state.count[index]}</span>
+        <button type="button" onClick={() => dispatch({ index, type: 'increment' })}>+1</button>
+        <button type="button" onClick={() => dispatch({ index, type: 'decrement' })}>-1</button>
+        <input value={index} onChange={(e) => setIndex(Number(e.target.value) || 0)} />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
