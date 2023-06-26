ae422ed0c0758dbf2e93ef038a67850328d5705c
daishi <daishi@axlight.com>
2023-06-26 13:46:41 +0900

    chore: update dev dependencies

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   launch: {
-    headless: process.env.HEADLESS !== 'false',
+    headless: process.env.HEADLESS === 'false' ? false : 'new',
     slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
   },
 };
